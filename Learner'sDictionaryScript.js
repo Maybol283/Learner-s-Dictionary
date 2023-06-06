6340d01e6c615ddf6b2ad66a77b00c88f9856f29
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2023-06-06 09:10:02 +0900

    Fixed Api issues

--- Learner'sDictionaryScript.js
+++ Learner'sDictionaryScript.js
@@ -4,26 +4,33 @@
 
 //begin a fetch to merriam webster API
 function merriamApiRequest(word){
- const url = `https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=06eed36a-18cf-4365-8cf8-5ae5edd1c537`;
+ const url = `https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=06eed36a-18cf-4365-8cf8-5ae5edd1c537`;
 
  fetch(url)
  .then(response => {
    if (!response.ok) {
-     throw new Error("Network response was not OK");
+     throw new Error('Network response was not OK');
    }
-   console.log( response.json());
+   return response.json();
  })
- .then(data => handleApiResponse(data))
+ .then(data => { 
+   console.log(data);
+   handleApiResponse(data);
+ })
  .catch(error => {
-   console.error("Error fetching data:", error);
-   alert("An error occurred while fetching the data. Please try again later.");
+   alert('An error occurred while fetching the data. Please try again later.');
  });
 }
-//query Headword/ Description and Example
 
-function handleApiResponse(data){
-  const headwordElement = document.getElementById('headword');
-  headwordElement.textContent = data.hwi.hw.value;
+
+function handleApiResponse(data) {
+  const headwordEle = document.getElementById('headword');
+ console.log(data[0].hwi.hw);
+  if (data[0].hwi.hw) {
+    headwordEle.textContent = data[0].hwi.hw;
+  } else {
+    alert("No Word Found!");
+  }
 }
 //Push headword to #headword
 //push definition to #definition
@@ -40,8 +47,8 @@ submission.addEventListener("submit", (e) => {
       return;
     } else {
         word.value = word.value.toLowerCase();
+        merriamApiRequest(word.value);
     }
-    merriamApiRequest(word);
-
+   
     document.getElementById("word").value = ""
 })
