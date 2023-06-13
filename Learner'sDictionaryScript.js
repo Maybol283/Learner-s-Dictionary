da8d68e11d3383112c7ef8d55be14b21bb3d21b1
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2023-06-13 18:19:33 +0900

    Added animation to information box

--- Learner'sDictionaryScript.js
+++ Learner'sDictionaryScript.js
@@ -4,90 +4,103 @@
 
 //begin a fetch to merriam webster API
 function merriamApiRequest(word){
- const url = `https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=06eed36a-18cf-4365-8cf8-5ae5edd1c537`;
-
- fetch(url)
- .then(response => {
-   if (!response.ok) {
-     throw new Error('Network response was not OK');
-   }
-   return response.json();
- })
- .then(data => { 
-  const searchResult = searchForKey(data[0].def, 't'); 
-   handleApiResponse(data, searchResult);
- })
- .catch(error => {
-   alert('An error occurred while fetching the data. Please try again later.');
- });
-}
-
-function searchForKey(object, key) {
-  let result = null;
-  
-  if (object instanceof Array) {
-    for (let i = 0; i < object.length; i++) {
-      result = searchForKey(object[i], key); // Pass the 'key' parameter
-      if (result) {
-        break;
-      }
+  const url = `https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=06eed36a-18cf-4365-8cf8-5ae5edd1c537`;
+ 
+  fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not OK');
     }
-  } else {
-    for (let prop in object) {
-      if (prop === key) {
-        result = object[prop];
-        break;
-      }
-
-      if (
-        object[prop] instanceof Object || // Check if the property is an object
-        object[prop] instanceof Array // Check if the property is an array
-      ) {
-        result = searchForKey(object[prop], key); // Pass both 'object[prop]' and 'key' parameters
-        if (result) {
-          break;
-        }
-      }
-    }
-  }
-  
-  return result ;
-}
-
-
-function handleApiResponse(data, searchResult) {
-console.log(data)
-const headWord = document.getElementById('headword');
-const definitionEle = document.getElementById('definition');
-const exampleEle = document.getElementById('example');
-
-  if (data[0].hwi.hw) {
-    headWord.textContent = data[0].hwi.hw;
-    definitionEle.textContent = data[0].shortdef[0];
-    exampleEle.textContent = searchResult;
-  } else {
-    alert("No Word Found!");
-  }
-}
-//Push headword to #headword
-//push definition to #definition
-//push example sentence to #example-sentence
-let submission = document.getElementById("inputForm");
-
-submission.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    let word = document.getElementById("word");
-
-    if(word.value == ""){
-      alert("Enter A Word!");
-      return;
-    } else {
-        word.value = word.value.toLowerCase();
-        merriamApiRequest(word.value);
-      
-    }
+    return response.json();
+  })
+  .then(data => { 
+    const searchResult = [];
+    searchResult.push(searchForKey(data, 't'));
+    searchResult.push(searchForKey(data, 'shortdef'));
+    searchResult.push(searchForKey(data, 'hw'));
+    handleApiResponse(searchResult);
+  })
+  .catch(error => {
+    alert('An error occurred while fetching the data. Please try again later.');
+  });
+ }
+ //Search an Object recursively for the given key, Only finds the first true answer
+ function searchForKey(object, key) {
+   let result = null;
    
-
-})
+   if (object instanceof Array) {
+     for (let i = 0; i < object.length; i++) {
+       result = searchForKey(object[i], key); // Recursively searches until result != null
+       if (result) {
+         break;
+       }
+     }
+   } else {
+     for (let prop in object) {
+       if (prop === key) {
+         result = object[prop];
+         break;
+       }
+ 
+       if (
+         object[prop] instanceof Object || // Check if the property is an object
+         object[prop] instanceof Array // Check if the property is an array
+       ) {
+         result = searchForKey(object[prop], key); // Pass both 'object[prop]' and 'key' parameters
+         if (result) {
+           break;
+         }
+       }
+     }
+   }
+   return result ;
+ }
+ 
+ 
+ function handleApiResponse(searchResult) {
+ const h1 = document.getElementById('headword');
+ const d1 = document.getElementById('definition');
+ const e1 = document.getElementById('example');
+ const info = document.getElementById('information');
+ 
+   info.classList.toggle("slideOff");
+   setTimeout(() => {
+   
+   h1.textContent = searchResult[2];
+   d1.textContent = searchResult[1];
+   e1.textContent = searchResult[0];
+   
+     
+   info.classList.toggle("slideOff");
+   info.classList.toggle("slideOn");
+   }, 1000);
+   
+   setTimeout(() => {
+     info.classList.toggle("slideOn")
+   }, 5000);
+   
+ 
+   
+ }
+ 
+ 
+ let submission = document.getElementById("inputForm");
+ 
+ submission.addEventListener("submit", (e) => {
+     e.preventDefault();
+     
+     let word = document.getElementById("word");
+ 
+     if(word.value == ""){
+       alert("Enter A Word!");
+       return;
+     } else {
+         word.value = word.value.toLowerCase();
+         merriamApiRequest(word.value);
+       
+     }
+    
+ 
+ })
+ 
 
